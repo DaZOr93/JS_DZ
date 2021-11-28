3ba5ada2c3d6d0eae462c292ce6fbe955502b616
Vitalik <dazorchik@gmail.com>
2021-11-28 19:55:24 +0200

    dz3

--- js/script.js
+++ js/script.js
@@ -1,175 +1,134 @@
-// 1. С помощью функции prompt спросите у пользователя его страну проживания. Если пользователь ничего не ввел (пустая строка),
-// выведите сообщение "Введите данные", если пользователь ввел число, выведите сообщение "Название не может быть числом";
-let userCountry = prompt("Введите страну проживания");
-if (userCountry.trim() === ''){
-    alert("Введите данные");
-} else if(!isNaN(userCountry) ){
-    alert("Название не может быть числом");
+// 1. Создайте функцию, которая проверяет является ли значние числом. Функция должна возвращать Boolean.
+function checkNumber(a) {
+    return isFinite(a);
 }
-// 2. С помощью prompt попросите пользователя ввести два числа. После чего выведите результат является ли второе число
-// кратным первому числу;
-let a = prompt ("Ведите первое число");
-let b = prompt ("Ведите второе число");
-if (a % b === 0){
-    alert('Второе число кратно первому');
-}
-if (a % b !== 0){
-    alert('Второе число не кратно первому');
-}
-// 3. С помощью prompt попросите пользователя ввести число. Сделать проверку на пустую строку и если значение является НЕ числом.
-//     В результате вывести пользователю каким является число (четное или нечетное);
-let c = prompt("Введите число");
-if ((c.trim() !== '')&&(!isNaN(c))){
-    if(c%2 === 0){
-        alert(`Число ${c} четное`);
-    } else {
-        alert(`Число ${c} не четное`);
-    }
-}
-// 4. Создайте скрипт, который просит ввести пользователя число от 1 до 100. Добавить проверку на то, что значние является числом,
-//     на пустую строку, и на заданный диапазон (не меньше 1 и не больше 100). Выведите пользователю ответ в какой четверти лежит число.
-// 1-25 (первая четверь), 26-50 (вторая четверть), 51-75 (третья четверть), 76-100 (четвертая четверть);
-let d = prompt("Ведите число от 1 до 100");
-if ((!isNaN(d))&&(d.trim() !== '')&&(d>0)&&(d<=100)){
-    if((d>0)&&(d<=25)){
-        alert(`Число лежит ${d} в первой четверти`);
-    }
-    if((d>=26)&&(d<=50)){
-        alert(`Число лежит ${d} во второй четверти`);
-    }
-    if((d>=51)&&(d<=75)){
-        alert(`Число лежит ${d} в третей четверти`);
-    }
-    if((d>=75)&&(d<=100)){
-        alert(`Число лежит ${d} в четвертой четверти`);
-    }
-}
-// 5. Создайте скрипт, который в цикле будет выводить простые числа от 1 до 500;
-for (let i = 2; i<=500; i++) {
-    let prime = i;
-    for (let j = 2; j < i; j++) {
-        if (i % j === 0) {
-            prime = null
-        }
-    }
-    if (prime) {
-        console.log(prime);
-    }
-}
-// 6. Создайте скрипт, который выведет числа от 1000 до 300 в обратном порядке;
-for (let i = 1000; i >= 300; i--){
-    console.log(i);
-}
-// 7. Попросите пользователя ввести число. Выведите результат сложения, вычетания, деления и умножения введенного числа на все
-// числа от 1 до 100.
-let e = prompt("Введите число");
-if ((!isNaN(e))&&(e.trim() !== '')){
-    for (let i = 1; i <= 100; i++) {
-        console.log(`Сумма числа ${e} и ${i} равна ${+e + i}`);
-        console.log(`Разность числа ${e} и ${i} равна ${+e - i}`);
-        console.log(`Деление числа ${e} и ${i} равна ${+e / i}`);
-        console.log(`Умножение числа ${e} и ${i} равна ${+e * i}`);
-    }
-}
-// 8. Создайте простой калькулятор. Попросите пользователя первый операнд, знак и второй операнд. Отобразите пользователю результат
-// выполнения математического действия. Например: ввели 10, ввели "+", ввели 20 (3 вызова prompt), результат - 30 (вывести в alert).
-// Знаки могут быть "+", "-", "*", "/". Так же добавьте проверки на ввод двух операндов и знака. При делении добавьте обработку
-// деления на ноль. Если происходит деление на ноль - вывести сообщение "На 0 делить нельзя".
-let firstOperand = prompt("Введите первый операнд");
-let mathematicalSign = prompt("Введите Знак");
-let secondOperand = prompt("Введите второй операнд");
-if ((!isNaN(firstOperand))&&(firstOperand.trim() !== '')&&
-    (!isNaN(secondOperand))&&(secondOperand.trim() !== '')&&
-    (mathematicalSign === '+')||(mathematicalSign === '-')||(mathematicalSign === '*')||(mathematicalSign === '/')){
-    if(mathematicalSign === '+'){
-        alert(`${firstOperand} ${mathematicalSign} ${secondOperand} = ${+firstOperand + +secondOperand}`);
-    }
-    if(mathematicalSign === '-'){
-        alert(`${firstOperand} ${mathematicalSign} ${secondOperand} = ${+firstOperand - +secondOperand}`);
-    }
-    if(mathematicalSign === '*'){
-        alert(`${firstOperand} ${mathematicalSign} ${secondOperand} = ${+firstOperand * +secondOperand}`);
-    }
-    if((mathematicalSign === '/')&&(+secondOperand !==0)){
-        alert(`${firstOperand} ${mathematicalSign} ${secondOperand} = ${+firstOperand / +secondOperand}`);
-    }
-    if((mathematicalSign === '/')&&(+secondOperand ===0)){
-        alert('На 0 делить нельзя');
-    }
-}
-// 9. Создайте игру "Угадай число". Попросите пользователя ввести сумму денег, которую он хочет поставить на игру. Попросите
-// ввести диапазон чисел, в котором он хочет играть (например, от a до b). Выведите сумму, которую получит пользователь в том
-// случае, если угадает число. Сумма приза рассчитывается по формуле (b - a) * 0.1 + введенная пользователем сумма денег.
-//     После чего запросите пользователя ввести число, добавьте проверку на заданный пользователем в начале диапазон (от а до b).
-// После ввода пользователем числа, сформируйте в скрипте рандомное число в заданном пользователем диапазоне (от a до b)
-// (погуглите как получить рандомное число от числа до числа) и сравните введное пользователем число и сформированное рандомное
-// скриптом число. Если числа совпали, выведите в alert текст приветствия и сумму выигрыша, иначе сообщите о поражении и
-// проигранное сумме. Не забудьте проверки типов данных и пустой строки на каждом пользовательском вводе;
-let stakeAmount;
-let rangeStart;
-let rangeEnd;
-let prize;
-let bet;
-let winningNumber;
-stakeAmount = prompt("Введите сумму ставки");
-if ((!isNaN(stakeAmount)) && (stakeAmount !== '') && (stakeAmount !== ' ') && (stakeAmount !== null)) {
-    rangeStart = prompt("Видите начало диапазона");
-    if ((!isNaN(rangeStart)) && (rangeStart !== '') && (rangeStart !== ' ') && (rangeStart !== null) && (Number.isInteger(rangeStart))) {
-        rangeEnd = prompt("Видите конец диапазона");
-        if ((!isNaN(rangeEnd)) && (rangeEnd !== '') && (rangeEnd !== ' ') && (rangeEnd !== null) && (rangeStart < rangeEnd) && (Number.isInteger(rangeEnd))) {
-            prize = (+rangeEnd - +rangeStart) * 0.1 + +stakeAmount;
-            bet = prompt(`Введите число от ${rangeStart} до ${rangeEnd} . Призовой фонд ${prize} грн`)
-            if ((!isNaN(bet)) && (bet !== '') && (bet !== ' ') && (bet !== null) && ((bet >= rangeStart) && (bet <= rangeEnd)) && (Number.isInteger(bet))) {
-                winningNumber = Math.round(+rangeStart + Math.random() * (+rangeEnd + 1 - +rangeStart));
-                if (+bet === winningNumber) {
-                    alert(`ПОЗДРАВЛЯЕМ ВЫ ВЫГРАЛИ ${prize} ГРН`)
-                } else {
-                    alert(`К сожалению вы проиграли ${stakeAmount} грн`)
-                }
-            }
-            if ((!isNaN(bet)) && (bet !== '') && (bet !== ' ') && (bet !== null) && ((bet < rangeStart) || (bet > rangeEnd))) {
-                alert("Ставка не в ходит в диапазон")
-            }
-        }
-        if ((!isNaN(rangeEnd)) && (rangeEnd !== '') && (rangeEnd !== ' ') && (rangeEnd !== null) && (rangeStart > rangeEnd)) {
-            alert("Конец диапазона меньше чем начало")
-        }
-    }
-}
-// 10. Создайте игру "Камень-ножницы-бумага". Попросите пользователя ввести строкой одно из значений. Проверьте правильность ввода.
-//     Далее в скрипте напишите логику рандомного выбора одного из элементов игры и отобразите этот элемент пользователю.
-//     Введенный результат пользователя и сформированный скриптом рандомный результат обработайте по правилам игры.
-//     В зависимости от результата выведите пользователю сообщенение о том выиграл он или проиграл.
-//     Например: пользователь вводит Бумага, скрипт выводит Камень (рандомно сформированный результат), пользователю выводится
-// сообщение о победе;
-let handValue;
-let randHand;
-let hands = ['камень', 'ножницы', 'бумага']
-handValue = prompt("Введите камень, ножницы или бумага");
-handValue = handValue.trim();
-handValue = handValue.toLowerCase();
-if ((handValue === 'камень') || (handValue === 'ножницы') || (handValue === 'бумага')) {
-    if (handValue === 'камень') {
-        handValue = 1;
-    }
-    if (handValue === 'ножницы') {
 
-        handValue = 2;
+console.log(checkNumber(' 1d'));
+// 2. Создайте фукнцию, которая принимает 3 числа. Возвращать должна среднее из этих 3х чисел. (Не забудьте добавить проверки на
+// то, что аргументы не пустые и являются числами).
+function mean(number1, number2, number3) {
+    if (Emptiness(number1) || (!checkNumber(number1))) {
+        return "Первый аргумент пустой или не число"
     }
-    if (handValue === 'бумага') {
-
-        handValue = 3;
+    if (Emptiness(number2) || (!checkNumber(number2))) {
+        return "Второй аргумент пустой или не число"
     }
-    randHand = Math.floor(1 + Math.random() * (3 + 1 - 1));
-    if (((handValue === 1) && (randHand === 2)) ||
-        ((handValue === 2) && (randHand === 3)) ||
-        ((handValue === 3) && (randHand === 1))) {
-        alert(`Поздравляем вы выиграли!!! У вас ${hands[handValue - 1]} у компьютера ${hands[randHand - 1]}`);
-    } else if (((handValue === 1) && (randHand === 1)) ||
-        ((handValue === 2) && (randHand === 2)) ||
-        ((handValue === 3) && (randHand === 3))) {
-        alert(`Ничья!!! У вас ${hands[handValue - 1]} у компьютера ${hands[randHand - 1]}`);
-    } else {
-        alert(`Вы проиграли!!! У вас ${hands[handValue - 1]} у компьютера ${hands[randHand - 1]}`);
+    if (Emptiness(number3) || (!checkNumber(number3))) {
+        return "Третий аргумент пустой или не число"
     }
+    const sum = number1 + number2 + number3;
+    const max = Math.max(number1, number2, number3);
+    const min = Math.min(number1, number2, number3);
+    return sum - max - min;
 }
+
+function Emptiness(str) {
+    return ((str === '') || (str === ' '));
+}
+
+console.log(mean(6, 5, 1));
+
+// 3. Создайте фукнцию, которая принимает объект и проверяет есть ли в нем свойство 'name'. Вернуть Boolean тип.
+function findName(obj) {
+    return "name" in obj;
+}
+
+let user = {
+    name: 'Viktor',
+    username: 'Admin',
+    age: 25,
+    city: "Kramatorsk"
+};
+console.log(findName(user));
+
+// 4. Создайте функцию, которая принимает объект со свойствами name и username и возвращает строку в формате Имя Фамилия.
+function getFullName(obj) {
+    return `${obj.name} ${obj.username}`
+}
+
+console.log(getFullName(user));
+// 5. Создайте функцию, которая принимает объекте типа {name1: 200, name2: 500, name3: 400}. Функция должна вернуть сумму всех
+// свойств объекта.
+function sumObjectProperties(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        sum = obj[key] + sum;
+    }
+    return sum;
+}
+
+console.log(sumObjectProperties({name1: 200, name2: 500, name3: 400}));
+// 6. Создайте функцию, которая принимает объект. Все ЧИСЛОВЫЕ свойства объекта нужно поделить на 2,
+//     если получилось НЕ целое число - округлить его по математическим правилами, а текстовые поменять на строку
+// Hello, Palmo. Если свойство не числовое или не текстовое - удалить его из объекта. Функция должна вернуть переделанный объект.
+function editObject(obj) {
+    for (let key in obj) {
+        if (!Emptiness(obj[key]) && (checkNumber(obj[key]))) {
+            obj[key] = Math.round(obj[key] / 2);
+        } else if (typeof (obj[key]) === "string") {
+            obj[key] = "Hello, Palmo.";
+        } else {
+            delete obj[key];
+        }
+
+    }
+    return obj;
+}
+
+console.log(editObject({name1: ['s', 1], name2: 9, name3: 's00', name5: '400'}));
+// 7. Создайте игру "Викторина". Опираясь на ваши знания по JS, создайте игру, которая поочередно задаст пользователю 10 вопросов.
+//     За каждый правильный ответ начисляйте пользователю 1 балл. После ответа на все вопросы выведите сколько баллов заработал пользователь.
+function quiz(obj) {
+    let quizPoints = 0;
+    for (let key in obj) {
+        if (confirm(obj[key].question) === obj[key].answer) {
+            quizPoints++;
+        }
+    }
+    alert(`Вы заработали ${quizPoints} баллов!`)
+    return quizPoints;
+}
+
+const QUIZ_QUESTION = {
+    question1: {
+        question: "Правда ли, что черепахи плачут?",
+        answer: true,
+    },
+    question2: {
+        question: "Правда ли, что сказку «Цветик-семицветик» написал Пушкин?",
+        answer: false,
+    },
+    question3: {
+        question: "Правда ли, что самые первые машины были двухколесными? ",
+        answer: false,
+    },
+    question4: {
+        question: "Правда ли, что крокодилы могут притворяться бревнами, спасаясь от врагов?",
+        answer: false,
+    },
+    question5: {
+        question: "Правда ли, что Австралия – это самый большой материк?",
+        answer: false,
+    },
+    question6: {
+        question: "Правда ли, что блины выпекают из ржаной муки? ",
+        answer: false,
+    },
+    question7: {
+        question: "Правда ли, что Марс ближайшая к Солнцу планета?",
+        answer: false,
+    },
+    question8: {
+        question: "Правда ли, что на Земле больше соленой воды, чем пресной?",
+        answer: true,
+    },
+    question9: {
+        question: "Правда ли, что шакал является родственником собаки?",
+        answer: true,
+    },
+    question10: {
+        question: "Правда ли, что для производства стекла используют глину?",
+        answer: false,
+    },
+}
+quiz(QUIZ_QUESTION);
